157eb0567efbc0b768037f9a03e62c4654a139d5
erolmag <magveg@spray.se>
2016-05-11 14:16:31 +0200

    Rolfs upload efter ändring av model
    Model och ignore ändrade

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
 
-#test
 class Product(models.Model):
     name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -13,9 +12,3 @@ class Product(models.Model):
 class DetailedProductImage(models.Model):
     product = models.ForeignKey(Product)
     image = models.ImageField()
-
-class DetailedProductImage2(models.Model):
-    product2 = models.ForeignKey(Product)
-    image2 = models.ImageField()
-
-   #testingjhvghjgkhghdfgdfgsd
